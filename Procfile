4ea6be435536e4e1f461645aa7a4590e0c88e546
eggrollius <eggrollius@protonmail.com>
2023-10-18 11:25:59 -0400

    corrected __init__ for local testing aswell

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: python app/init.py
 web: python app.py
--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-# Configure database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
 
-# Import models
-from models import Course, MeetingInfo
+    # Configure database url
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or'sqlite:///site.db'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+    from .models import Course, MeetingInfo
 
-if __name__ == "__main__":
-    app.run()
+    @app.route("/")
+    def index():
+        return "Hello, World!"
+
+    return app
