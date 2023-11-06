7ee3f664ad3c0f2bd61672bf0d2149a14359bb13
eggrollius <eggrollius@protonmail.com>
2023-11-06 17:18:43 -0500

    Schedule generator and  viewer added.

--- app/static/index.js
+++ app/static/index.js
@@ -1,8 +1,11 @@
 let departments = [];
 let courseCodes = [];
 let courseData = {};
-let sections = [];
+let sections = {};
 let schedules = [];
+
+let currentScheduleIndex = 0; // This will keep track of the current schedule index
+let totalSchedules = 1000; // Total number of schedules (for schedule navigation)
 
 function init(){
     //add the departments
@@ -26,6 +29,36 @@ function init(){
         
     });
 
+    // Add event listeners for schedule navigation
+    // For previous button
+    document.getElementById('previous').addEventListener('click', function(event) {
+        event.preventDefault();
+        if (currentScheduleIndex > 0) {
+            currentScheduleIndex--;
+          renderSchedule(currentScheduleIndex);
+        }
+      });
+    // For next butotn
+    document.getElementById('next').addEventListener('click', function(event) {
+    event.preventDefault();
+    if (currentScheduleIndex < totalSchedules - 1) {
+        currentScheduleIndex++;
+        renderSchedule(currentScheduleIndex);
+    }
+    });
+    // For direct schedule index input navigatoin
+    document.getElementById('scheduleIndexInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        let inputIndex = parseInt(this.value, 10) - 1; // Arrays are zero-indexed
+        if (inputIndex >= 0 && inputIndex < totalSchedules) {
+            currentScheduleIndex = inputIndex;
+        renderSchedule(currentScheduleIndex);
+        } else {
+        alert('Please enter a valid schedule number.');
+        }
+    }
+    });
+
 }
 
 //get course data async, then update ui, and local data.
@@ -46,6 +79,7 @@ function add_course(department, course_code) {
                         courseCodes.push(courseName); 
                         generateCourseAccordion(courseName);
                         generateSchedules();
+                        renderSchedule(currentScheduleIndex);
                     } else if(xhr.status == 400){
                         alert("Course does not Exist");
                     } else {
@@ -162,8 +196,6 @@ function addSectionCard(section, courseName) {
     accordionBody.appendChild(sectionCard);
 }
 
-
-
 class Meeting {
     constructor(startDate, endDate, daysOfWeek, timeInterval) {
       this.startDate = new Date(startDate);
@@ -195,26 +227,15 @@ class Meeting {
         return true;
     }
 
-    mergeWith(other) {
-        if (this.hasConflictWith(other)) {
-          throw new Error('Cannot merge conflicting TimeObjects');
-        }
-    
-        const newStartDate = this.startDate < other.startDate ? this.startDate : other.startDate;
-        const newEndDate = this.endDate > other.endDate ? this.endDate : other.endDate;
-    
-        const newDaysOfWeek = Array.from(new Set([...this.daysOfWeek, ...other.daysOfWeek]));
-    
-        const [start1, end1] = this.timeInterval.split('-');
-        const [start2, end2] = other.timeInterval.split('-');
-        const newStart = start1 < start2 ? start1 : start2;
-        const newEnd = end1 > end2 ? end1 : end2;
-    
-        const newTimeInterval = `${newStart}-${newEnd}`;
-    
-        return new Meeting(newStartDate, newEndDate, newDaysOfWeek, newTimeInterval);
-      }
-    
+    clone() {
+        return new Meeting(
+            this.startDate.toISOString(),
+            this.endDate.toISOString(),
+            [...this.daysOfWeek], // Shallow copy should suffice for array of strings
+            this.timeInterval
+        );
+    }
+
 }
 
 class Section {
@@ -228,7 +249,8 @@ class Section {
         const [parentStartDate, parentEndDate] = parentMeetingInfo.meeting_date.split(' to ');
         const parentDaysOfWeek = parentMeetingInfo.days.split(' ');
 
-        this.meeting = new Meeting(parentStartDate, parentEndDate, parentDaysOfWeek, parentMeetingInfo.time);
+        this.meetings = [];
+        this.meetings.push(new Meeting(parentStartDate, parentEndDate, parentDaysOfWeek, parentMeetingInfo.time));
         
         if (this.child) {
             const childMeetingInfo = this.child.meeting_infos[0];
@@ -237,53 +259,192 @@ class Section {
 
             const childMeeting = new Meeting(childStartDate, childEndDate, childDaysOfWeek, childMeetingInfo.time);
       
-            this.meeting = this.meeting.mergeWith(childMeeting);
+            this.meetings.push(childMeeting);
         }
       
     }
+
+    clone() {
+        return new Section(
+            JSON.parse(JSON.stringify(this.parent)), // Deep copy for 'parent'
+            JSON.parse(JSON.stringify(this.child)), // Deep copy for 'child'
+        );
+    }
 }
 
-class schedule {
+class Schedule {
     constructor() {
         this.sections = [];
-        this.meeting = null;
+        this.meetings = [];
     }
-
+    // Takes a section object
     tryAddSection(other) {
-        if(!this.meeting.hasConflictWith(other.meeting)) {
+        if(this.meetings.length <= 0) {
             this.sections.push(other);
-            this.meeting = this.meeting.mergeWith(other);
+            this.meetings.push(...other.meetings);
             return true;
-        } else {
-            return false;
         }
 
+        for(let i = 0; i < this.meetings.length; i++) {
+            for(let j = 0; j < other.meetings.length; j++) {
+                if(this.meetings[i].hasConflictWith(other.meetings[j])) {
+                    return false;   
+                }
+            }
+
+        }
+
+        this.sections.push(other);
+        this.meetings.push(...other.meetings);
+        return true;
+
     }
+
+    clone() {
+        const newSchedule = new Schedule();
+        newSchedule.sections = this.sections.map(section => section.clone()); // Deep copy each section
+        // Ensure meetings are deep copied, assuming each 'meeting' has a clone method
+        newSchedule.meetings = this.meetings.map(meeting => meeting.clone()); // Deep copy each meeting
+        return newSchedule;
+    }
+}
+
+function clearSchedule() {
+    // List of all days in a week
+    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    // Loop through each day and clear its contents
+    daysOfWeek.forEach(day => {
+        const dayColumn = document.getElementById(day);
+        while (dayColumn.hasChildNodes()) {
+            dayColumn.removeChild(dayColumn.firstChild);
+        }
+    });
 }
 
 function generateSchedules() {
     for(const courseCode of courseCodes) {
-        generateAllSections(courseData[courseCode]);
+        generateAllSections(courseCode);
     }
+    console.log(sections);
+
+    schedules = [new Schedule()]; // Creates new list of schedules, contains 1 empty schedule
+    for(const courseCode of courseCodes) { 
+        // For each required course code
+        currentCourseSections = sections[courseCode];
+        let newSchedules = []; 
+        for(const section of currentCourseSections) {
+            // For each possible way of taking the course code
+            let bufferSchedule;
+            for(const schedule of schedules) {
+                // For each old schedule
+                // Make a new schedule by adding each possible way of taking the course
+                bufferSchedule = schedule.clone();
+                if(bufferSchedule.tryAddSection(section)) {
+                    // If no conflict
+                    newSchedules.push(bufferSchedule);
+                    console.log('no conflict found');
+                } else {
+                    // Conflict
+                    console.log(`Conflict found with: ${courseCode + section.parent.section} and this schedule:`);
+                    console.log(bufferSchedule);
+                }
+                console.log(newSchedules.length);
+            }
+        }
+        if(newSchedules.length > 0) {
+            schedules = newSchedules;
+        } else {
+            console.log(`Could not add this course: ${courseCode}`);
+        }
+        
+    }
+
+    totalSchedules = schedules.length;
+    console.log(schedules)
 }
 
-function generateAllSections(course) {
+
+
+function renderSchedule(scheduleIndex){
+    // Clear old schedule
+    clearSchedule();
+    // Iterate over the schedule and update the calendar cells
+    let schedule;
+    schedule = schedules[scheduleIndex];
+    schedule.sections.forEach(section => {
+        // Do parent class first:
+        let c = section.parent; // c for 'class' (class is restricted variable name)
+        let meeting = c.meeting_infos[0];
+        let [startTime, endTime]  = meeting.time.split(' - ')
+        let days = meeting.days.split(' ');
+        days.forEach(day => {
+            // Get column for this day
+            const dayColumn = document.getElementById(day);
+            // Create and add block for this class
+            const classBlock = document.createElement('div');
+            classBlock.classList.add('class-block');
+            classBlock.innerHTML = `${c.course_code + ' ' + c.section}<br>${meeting.time}`;
+
+
+            classBlock.style.top = timeToPixels(startTime) + 'px';
+            classBlock.style.height = durationToPixels(startTime, endTime) + 'px';
+            dayColumn.appendChild(classBlock);
+        });
+
+        // Do child class now:
+        if(section.child != null){
+            c = section.child; // c for 'class' (class is restricted variable name)
+            meeting = c.meeting_infos[0];
+            [startTime, endTime]  = meeting.time.split(' - ')
+            days = meeting.days.split(' ');
+            days.forEach(day => {
+                // Get column for this day
+                const dayColumn = document.getElementById(day);
+                // Create and add block for this class
+                const classBlock = document.createElement('div');
+                classBlock.classList.add('class-block');
+                classBlock.innerHTML = `${c.course_code + ' ' + c.section}<br>${meeting.time}`;
+
+    
+                classBlock.style.top = timeToPixels(startTime) + 'px';
+                classBlock.style.height = durationToPixels(startTime, endTime) + 'px';
+                dayColumn.appendChild(classBlock);
+            });
+        }
+    });
+}
+
+function generateAllSections(courseCode) {
+    const course = courseData[courseCode];
     const courseSections = course.sections;
-    console.log(courseSections);
+
+    sections[courseCode] = [];
+
     for(const sectionName of Object.keys(courseSections)) {
         const section = courseSections[sectionName];
-        console.log(section);
         if(section.children.length <= 0) {
             const newSection = new Section(section.parent);
-            sections.append(newSection);
+            sections[course.courseName].append(newSection);
         } else {
             for(const child of section.children) {
                 const newSection = new Section(section.parent, child);
-                sections.push(newSection);
+                sections[courseCode].push(newSection);
             }
         }
     }
-    console.log(sections);
 }
 
+// Function to convert time ("HH:MM") to pixels
+function timeToPixels(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return (hours + minutes / 60 - 7) * 50; // 50 pixels per hour, adjust the -7 based on start of time column
+  }
+  
+  // Function to calculate the duration in pixels
+  function durationToPixels(startTime, endTime) {
+    const startTimeInPixels = timeToPixels(startTime);
+    const endTimeInPixels = timeToPixels(endTime);
+    return endTimeInPixels - startTimeInPixels;
+  }
 init();
